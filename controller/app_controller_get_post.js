7d3d750cc1499b370c9a64d21f6b3c10dd168204
Roberto Goddard <mgroberto38@hotmail.com>
2025-11-06 01:56:26 -0500

    new

--- controller/app_controller_get_post.js
+++ controller/app_controller_get_post.js
@@ -4,6 +4,6 @@ export async function get_post(req, res) {
   const { data, error } = await supabase
     .from("noticias")
     .select("title, description");
-    console.log(data)
+    
   res.send(data);
 }
