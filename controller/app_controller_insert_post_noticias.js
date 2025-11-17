783ac13b09b3c250867a5bf2f04744594ac0246a
Roberto Goddard <mgroberto38@hotmail.com>
2025-11-17 18:41:37 -0500

    new

--- controller/app_controller_insert_post_noticias.js
+++ controller/app_controller_insert_post_noticias.js
@@ -4,9 +4,16 @@ export async function insert_post(req, res) {
   try {
     const title = req.body.title;
     const description = req.body.description;
+    const createBy = req.body.createBy;
+    const timeCreated = req.body.timeCreated;
     const { error } = await supabase
       .from("noticias")
-      .insert({ title: title, description: description });
+      .insert({
+        title: title,
+        description: description,
+        createBy: createBy,
+        timeCreated: timeCreated,
+      });
   } catch {
     res.status(400).send("Los datos ingresados estan erroneos");
   }
